33596cfcf0a7af976eacc810f350591f763390c7
Dev Chaudhari <devchaudhari.231cs221@nitk.edu.in>
2024-12-13 21:15:25 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path= require('path');
 const multer=require('multer');
 const express=require("express");
 const Route= require('./routes/routes.js');
-const apiUrl="mongodb://localhost:27017/mvc";
+const apiUrl=/**"mongodb://localhost:27017/mvc";/*/"mongodb+srv://yogayogayoga:yogayogayoga@cluster0.2k0hk.mongodb.net/testdb?retryWrites=true&w=majority&appName=Cluster0" ;
 mongoose.connect(apiUrl).then(()=> console.log("Connected to mongodb"))
 .catch(err => console.log("Failed to connect to mongodb",error));
 const app = express();
@@ -12,9 +12,19 @@ const PORT = 3000;
 const client =mongoose.connect(apiUrl)
 .then(response=> console.log("Connected to mongodb database"))
 .catch(error=> console.log("Failed to connect",error));
+app.use(express.json());
+
+app.use(express.urlencoded({extended:true})); 
+
 app.use('/',Route);
+
 app.use(express.static(path.join(__dirname,'views')));
+
+
+
 //app.use(express.static(path.join(__dirname,'mvcmodel')));
 app.listen(PORT,()=>{
-console.log(`Mongodb is running on http://localhost:${PORT} the users are in http://localhost:${PORT}/users http://localhost:${PORT}/user/image`);
+console.log(`Mongodb is running on http://localhost:${PORT} the users are in http://localhost:${PORT}/users
+  http://localhost:${PORT}/user/image .
+  Login window= http://localhost:${PORT}/login . Register window = http://localhost:${PORT}/register`);
 });
