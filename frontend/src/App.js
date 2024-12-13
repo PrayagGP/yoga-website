e3fb2bd67f79a47fb237a40ec5318fb4d1beb1ae
Khushisesma <130694683+Khushisesma@users.noreply.github.com>
2024-12-13 22:24:52 +0530

    frontend initialize

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+
+function App() {
+
+  return (
+    <div>
+      <h1>Yoga Website
+      </h1>     
+    </div>
+  );
+}
+
+export default App;
