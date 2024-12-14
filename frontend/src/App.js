28048a2c8ab2cb19f9364352f9258bc142e6b4a6
Khushisesma <130694683+Khushisesma@users.noreply.github.com>
2024-12-14 16:28:36 +0530

    header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React from "react";
+import Header from "./components/Header.js";
+import Footer from "./components/Footer.js";
 
 function App() {
-
   return (
     <div>
-      <h1>Yoga Website
-      </h1>     
+      <Header />
+      <h1>Yoga Website</h1>
+      <Footer /> {/* Footer uses Link, so this works now */}
     </div>
   );
 }
