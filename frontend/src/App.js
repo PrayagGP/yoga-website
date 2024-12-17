e30c52a388fc03f21becc06b0667eb4dc1fa16db
Khushisesma <130694683+Khushisesma@users.noreply.github.com>
2024-12-17 21:03:02 +0530

    Event page initialised

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import Header from "./components/Header.js";
 import Footer from "./components/Footer.js";
 import Homepage from "./components/homepage.js";
+import Events from "./components/EventsPage.js";
 
 function App() {
   return (
     <div>
       <Header />
-      <Homepage />
+      <Events />
       <Footer /> {/* Footer uses Link, so this works now */}
     </div>
   );
