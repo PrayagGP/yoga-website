c90531c2613bb8bc246a587cefa21418eda1a51d
Khushisesma <130694683+Khushisesma@users.noreply.github.com>
2025-01-20 03:41:43 +0530

    team front page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,13 @@ import Footer from "./components/Footer.js";
 import Homepage from "./components/homepage.js";
 import Events from "./components/EventsPage.js";
 import EventsList from "./components/EventList.js";
+import TeamcatogPage from "./components/teamcatogPage.js";
 
 function App() {
   return (
     <div>
       <Header />
-      <EventsList />
+      <TeamcatogPage />
       <Footer /> {/* Footer uses Link, so this works now */}
     </div>
   );
