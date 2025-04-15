c9f8c1f5ed130312015a5da2ce8e0843df9b3531
Nischal Basavaraju <nischal.basavaraju@gmail.com>
2025-04-15 20:42:45 +0530

    Resolve merge conflicts with prashant-frontend

--- frontend/src/components/homepage.js
+++ frontend/src/components/homepage.js
@@ -1,73 +1,11 @@
-<<<<<<< HEAD
-import React from "react";
-import "./HomePage.css";
-
-const Homepage = () => {
-=======
 import React from 'react';
 import './HomePage.css';
 
 function Homepage() {
->>>>>>> 2013c7f895fbeb786f63f9a81d3c67a6cf249f89
   return (
     <div className="homepage">
       {/* Hero Section */}
       <div className="hero">
-<<<<<<< HEAD
-        <div className="hero-overlay">
-          {/* <h1>Yoga Club</h1> */}
-          {/* <p>Explore mindfulness and holistic well-being</p> */}
-        </div>
-      </div>
-
-      {/* Event Categories */}
-      <div className="events">
-        <h1>Events</h1>
-        <div className="events-cards">
-          <div className="events-card events-1"></div>
-          <div className="events-card events-2"></div>
-          <div className="events-card events-3"></div>
-        </div>
-      </div>
-
-
-      {/* About Us Section */}
-      <div className="about-us">
-        <div className="about-content">
-          <h1>About Us</h1>
-          <p>
-            The Yoga Club at NITK is a vibrant community dedicated to promoting
-            physical well-being, mental clarity, and spiritual growth among
-            students and faculty. Rooted in the ancient traditions of yoga, the
-            club provides a serene space for individuals to explore the art of
-            mindfulness and holistic living amidst the dynamic campus life.
-            Whether you’re a beginner or an experienced practitioner, the Yoga
-            Club welcomes everyone to join this journey of self-discovery and
-            inner peace.
-          </p>
-        </div>
-        <div className="about-image"></div>
-      </div>
-
-      {/* Webinars Section */}
-      <div className="webinars">
-        <h1>Webinars</h1>
-        <div className="webinar-cards">
-          <div className="webinar-card webinar-1"></div>
-          <div className="webinar-card webinar-2"></div>
-          <div className="webinar-card webinar-3"></div>
-        </div>
-      </div>
-      <div className="hero">
-        <div className="hero-overlay">
-          <h1>Meet our Team</h1>
-          {/* <p>Explore mindfulness and holistic well-being</p> */}
-        </div>
-      </div>
-    </div>
-  );
-};
-=======
         <div className="hero-overlay"></div>
       </div>
 
@@ -129,6 +67,5 @@ function Homepage() {
     </div>
   );
 }
->>>>>>> 2013c7f895fbeb786f63f9a81d3c67a6cf249f89
 
 export default Homepage;
